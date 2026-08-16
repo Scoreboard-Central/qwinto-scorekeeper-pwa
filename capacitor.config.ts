691db40934e8fd8432db4a289728d645a0da0c0e
Blake Lamb <blake.lamb31@gmail.com>
2026-08-16 16:52:53 -0600

    fix(android): change package name to io.qwinto.app to match production app on Google Play

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.blakelamb.qwintoscorekeeper',
+  appId: 'io.qwinto.app',
   appName: 'Qwinto Scorekeeper',
   webDir: 'dist/qwinto-scorekeeper-pwa',
   server: {
